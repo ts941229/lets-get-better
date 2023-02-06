2aa1802bb9957198b6593e487dfd9208a8a566fc
taeseok <tsk941229@gmail.com>
2023-02-06 17:09:05 +0900

    encapsulation

---
+++ src/encapsulation/Capsule.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class Capsule {
+	int num;
+	
+	public Capsule(int num) {
+		this.num = num;
+	}
+    
+    public double getHalf(){
+        return num/2;
+    }
+}
---
+++ src/encapsulation/Main.java
@@ -0,0 +1,10 @@
+package encapsulation;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Capsule capsule = new Capsule(10);
+        System.out.println(capsule.getHalf());
+	}
+	
+}
